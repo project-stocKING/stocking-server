c3d7f6fb5e24223671f28506b079fa64aa7738d8
Michał Czarnecki <mich.czarnecki@o2.pl>
2016-03-21 16:48:08 +0100

    Reorganizacja v2

---
+++ src/main/java/ServerApp/ServerApplicationMain.java
@@ -0,0 +1,33 @@
+package ServerApp;
+
+import Database.DatabaseConnection;
+import Models.StockCompany;
+
+import java.net.UnknownHostException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+/**
+ * Created by Beata on 2016-03-17.
+ */
+public class ServerApplicationMain {
+
+    public static void main( String[] args ) throws ParseException {
+
+        DatabaseConnection database = new DatabaseConnection();
+        ArrayList<StockCompany> stockCompanyArrayList = new ArrayList<StockCompany>();
+
+        try {
+            database.connectToDatabase();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+       // stockCompanyArrayList = database.getCollection("PZU");
+
+        database.findDocByDate();
+    }
+
+
+
+}
