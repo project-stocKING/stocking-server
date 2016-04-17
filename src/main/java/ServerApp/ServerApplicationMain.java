8caa85cf22f003e1c4a4a0d5296658aec5cb8a33
Daniel Bilinski <209963@student.pwr.edu.pl>
2016-04-17 20:06:08 +0200

    change test

--- src/main/java/ServerApp/ServerApplicationMain.java
+++ src/main/java/ServerApp/ServerApplicationMain.java
@@ -20,13 +20,14 @@ public class ServerApplicationMain {
 
     public static void main( String[] args ) throws ParseException, UnknownHostException, FileNotFoundException {
 
-       // RequestListener requestListener = new RequestListener(5001);
-        IndexInformation indexInformation = new IndexInformation("ISMA", "KGHM");
+        RequestListener requestListener = new RequestListener(5001);
+        /*IndexInformation indexInformation = new IndexInformation("ISMA", "KGHM");
         indexInformation.addParameter("StartDate", "20120408");
         indexInformation.addParameter("endDate", "20120824");
         indexInformation.addParameter("period", "45");
         IndexManager indexManager = new IndexManager(indexInformation);
         String json = indexManager.calculateIndex();
+        */
     }
 
 }
